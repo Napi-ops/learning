ed1da2419b31232728e3aa0507f0ed0a003adb15
napi <napideep@outlook.com>
2019-11-12 08:18:30 +0800

    进行了一些删改

--- leap year.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-year = 2000
-while year > 1999 and year <2501:
-    if (year % 4 == 0 and year % 100 != 0) or year %400 == 0:
-        print(year,'是闰年')
-    else:
-        print(year,'不是闰年')
-    year = year + 1
